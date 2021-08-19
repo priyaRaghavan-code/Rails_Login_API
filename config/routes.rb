756e4bb3722b9b08df2b82177a1642e804acca35
priyaRaghavan-code <priyabhuvanaj99@gmail.com>
2021-08-19 21:19:46 +0530

    Login and Registration Api Created

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
+  root to: "static#home"
+  namespace :v1 do
+    resources :sessions, only:[:create]
+  end
+
+  namespace :v1 do
+    resources :registrations, only:[:create]
+  end
+  namespace :v1 do
+        match 'sessions/logout' => "sessions#logout",:via => :delete
+        match 'sessions/logged_in' => 'sessions#logged_in', :via => :get
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
