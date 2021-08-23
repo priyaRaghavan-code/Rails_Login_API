4edbd4d2c90a0979bb03faa52eabfc0657860933
priyaRaghavan-code <priyabhuvanaj99@gmail.com>
2021-08-23 20:46:28 +0530

    Indendation updated

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,18 +2,15 @@ class UsersController < ApplicationController
   before_action :authorize_request, except: :create
   before_action :find_user, except: %i[create index]
 
-  # GET /users
   def index
     @users = User.all
     render json: @users, status: :ok
   end
 
-  # GET /users/{username}
   def show
     render json: @user, status: :ok
   end
 
-  # POST /users
   def create
     @user = User.new(user_params)
     if @user.save
@@ -24,17 +21,15 @@ class UsersController < ApplicationController
     end
   end
 
-  # PUT /users/{username}
   def update
     if @user.update(user_params)
-      render json: @user, status: 200
+      render json: @user, status: :ok
     else
       render json: { errors: @user.errors.full_messages },
              status: :unprocessable_entity
     end
   end
 
-  # DELETE /users/{username}
   def destroy
    if @user.destroy
     render json: @user, status: :ok
@@ -57,4 +52,5 @@ class UsersController < ApplicationController
       :avatar, :name, :username, :email, :password, :password_confirmation
     )
   end
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,9 @@
 class User < ApplicationRecord
   has_secure_password
-  # mount_uploader :avatar, AvatarUploader
-  validates :email, presence: true, uniqueness: true
-  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :username, presence: true, uniqueness: true
-  validates :password,
-            length: { minimum: 6 },
-            if: -> { new_record? || !password.nil? }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_PASSWORD_REGEX = /\A(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}\z/i
+
+  validates :email, presence: true, length: { maximum: 300 },format: { with: VALID_EMAIL_REGEX },uniqueness: true
+  validates :password, presence: true,format: { with: VALID_PASSWORD_REGEX }
 end
