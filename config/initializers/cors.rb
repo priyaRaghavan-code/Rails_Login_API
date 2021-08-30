23ee925f5349691b99edb442ca14597fa0be7502
priyaRaghavan-code <priyabhuvanaj99@gmail.com>
2021-08-30 13:01:16 +0530

    Changes udpated

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     # origins 'http://localhost:3000'
-    origins 'https://auth-front-end.herokuapp.com'
+    origins 'https://auth123-front-end.herokuapp.com'
 
     resource '*',
       headers: :any,
