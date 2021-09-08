1756de1fabae142e5c00c3a4391045c1c05ddd8f
priyaRaghavan-code <priyabhuvanaj99@gmail.com>
2021-09-08 15:37:04 +0530

    Rspec completed

--- spec/requests/authentication_request_spec.rb
+++ spec/requests/authentication_request_spec.rb
@@ -1,23 +1,22 @@
 require 'rails_helper'
-
 RSpec.describe "Authentications", type: :request do
   describe 'POST auth/login' do
     let(:user) { create(:user) }
+
     it 'authenticates the user' do
       post '/auth/login', params: { email: user.email, password: 'User@123'}
-      puts(response.body,"Body")
-      expect(response).to have_http_status(:created)
+      expect(response).to have_http_status(:ok)
     end
 
     it 'returns error when email does not exist' do
-      post '/auth/login', params: { email: 'ac@gmail.com', password: 'password' }
-      expect(response).to have_http_status(:unauthorized)
+     post '/auth/login', params: { email: 'ac@gmail.com', password: 'password' }
+     expect(response).to have_http_status(:unauthorized)
     end
 
     it 'returns error when password is incorrect' do
       post '/auth/login', params: { username: user.email, password: 'incorrect' }
       expect(response).to have_http_status(:unauthorized)
     end
-
+    
   end
 end
