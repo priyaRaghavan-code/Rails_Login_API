d0f06f275f03f9d7911eadac2d45f9757ab4bb8b
priyaRaghavan-code <priyabhuvanaj99@gmail.com>
2021-09-30 13:04:07 +0530

    Env updated

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,9 +1,9 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     # origins '*'
-    origins 'http://localhost:3000'
+    # origins 'http://localhost:3000'
     # origins 'https://auth1234-front-end.herokuapp.com'
-    # origins Figaro.env.frontend_url
+    origins Figaro.env.frontend_url
 
     resource '*',
       headers: :any,
