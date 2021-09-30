c82b6e94630eae7e76d2b7b54da7dd779b2fdd99
priyaRaghavan-code <priyabhuvanaj99@gmail.com>
2021-09-30 13:33:26 +0530

    Unused files removed

--- db/migrate/20210914101549_create_orders.rb
+++
@@ -1,9 +0,0 @@
-class CreateOrders < ActiveRecord::Migration[6.1]
-  def change
-    create_table :orders do |t|
-      t.decimal :sub_total,null: false, precision:15, scale:2
-
-      t.timestamps
-    end
-  end
-end
