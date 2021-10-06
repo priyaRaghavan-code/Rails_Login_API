70f333a02e3d6304adc12cdefdbc7eadf8d72f28
priyaRaghavan-code <priyabhuvanaj99@gmail.com>
2021-10-06 13:35:41 +0530

    Cart file changes update

--- app/controllers/api/v1/cart_items_controller.rb
+++ app/controllers/api/v1/cart_items_controller.rb
@@ -1,28 +1,41 @@
 module Api
-  module V1
-    class CartItemsController < ApplicationController
+	module V1
+		class CartItemsController < ApplicationController
 
-        def create
-            chosen_product = Product.find_by(params[:product_id])
-            result = Cart::Actions::AddToCart.run(
-              cart_item_params,
-              chosen_product
-            )
+			def create
+				chosen_product = Product.find(params[:product_id])
+				# cart_item = CartItem.create(cart_item_params)
+				# chosen_product.cart_items << CartItem.new()
 
-            if result.success?
-              render json: {token: result.fixtures['cart_item'], status: :ok}
-            else
-              render json: { error: result.message }, status: :unauthorized
-            end
+        result = Cart::Actions::AddToCart.run(
+          chosen_product,
+          cart_item_params
+        )
+        if result.success?
+          render json: {token: result.fixtures['cart_item'], status: :ok}
+        else
+          render json: { error: result.message }, status: :unauthorized
         end
 
+			end
 
-        private
+			def add_quantity
+				cart_item = CartItem.find(params[:id])
+				cart_item.increment!(:quantity)
+				render json: {status: "Success", message: "Quantity incremented Successfully", data:chosen_product}
+			end
 
-        def cart_item_params
-          params.permit(:user_id)
-        end
-    end
-      
-  end
+			def reduce_quantity
+				cart_item = CartItem.find(params[:id])
+				cart_item.decrement!(:quantity)
+				render json: {status: "Success", message: "Quantity decremented Successfully", data:chosen_product}
+			end
+
+			private 
+			def cart_item_params
+				params.permit(:user_id, :quantity, :total)
+			end
+
+		end
+	end
 end
